34847e158a169e3f78448c70471a7d12c096f219
john <xiao-jianfeng@qq.com>
2015-12-19 14:58:41 +0800

    Fix typo, add base setting for single page app

--- single-page-app/gulpfile.js
+++ single-page-app/gulpfile.js
@@ -7,7 +7,10 @@ var gulp = require('gulp'),
 var versionFileDist = 'dist/';
 
 gulp.task('assetsVersionReplace', function () {
-  gulp.src(['css/*.css', 'js/*.js'])
+  gulp.src(['css/*.css', 'js/*.js']
+      // Setting `base:'.'` for keeping `js`,`css` folder structure in `dist`
+      , {base: '.'}
+    )
     .pipe(assetsVersionReplace({
       tsVersionedFilesDest: versionFileDist,
       replaceTemplateList: [
